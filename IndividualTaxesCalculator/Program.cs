2455e7eaa172c15196aa59c33438e5deaf167701
Adam Hodovanets <adamhodovanets@gmail.com>
2018-05-18 15:19:02 +0300

    - Tax calculation

--- IndividualTaxesCalculator/Program.cs
+++ IndividualTaxesCalculator/Program.cs
@@ -10,6 +10,40 @@ namespace IndividualTaxesCalculator
     {
         static void Main(string[] args)
         {
+            decimal tax;
+            decimal result = 0;
+            Console.WriteLine("Basic Salary: ");
+            decimal.TryParse(Console.ReadLine()?.Replace(",","."),out decimal basicSalary);
+            Console.WriteLine("Current Salary: ");
+            decimal.TryParse(Console.ReadLine()?.Replace(",", "."), out decimal currentSalary);
+            Console.WriteLine($"BS: {basicSalary} | CS: {currentSalary}");
+
+            if (currentSalary <= basicSalary)
+            {
+                result = currentSalary;
+            }
+            else if (currentSalary > basicSalary && currentSalary <= 5 * basicSalary)
+            {
+                result = currentSalary - (currentSalary * (decimal)0.15);
+            }
+            else if (currentSalary > 5 * basicSalary && currentSalary <= 10 * basicSalary)
+            {
+                result = OverProfit(basicSalary, currentSalary, 5, (decimal) 0.17);
+            }
+            else if (currentSalary > 10 * basicSalary)
+            {
+                result = OverProfit(basicSalary, currentSalary, 10, (decimal) 0.2);
+            }
+            Console.WriteLine($"Result: {result}");
+
+            Console.ReadKey();
+        }
+
+        static decimal OverProfit(decimal basicSalary,decimal currentSalary, decimal multiplier, decimal tax)
+        {
+            decimal salary = currentSalary - (currentSalary - 5 * basicSalary);
+            decimal overTen = (currentSalary - 5 * basicSalary);
+            return (salary - salary * (decimal)0.15) + (overTen - overTen * tax);
         }
     }
 }
